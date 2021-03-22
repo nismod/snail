ba9848891d553874ca474df418f5414188d268d2
Tom Russell <tomalrussell@gmail.com>
2021-03-22 17:26:46 +0000

    Python package scaffolding

---
+++ src/snail/__init__.py
@@ -0,0 +1,20 @@
+"""snail - the spatial networks impact assessment library
+"""
+import pkg_resources
+
+# Import things to define what is accessible directly on snail, when a client
+# writes::
+#   from snail import Something
+
+# e.g. uncomment:
+# from snail.network import Network
+
+try:
+    __version__ = pkg_resources.get_distribution(__name__).version
+except pkg_resources.DistributionNotFound:
+    __version__ = 'unknown'
+
+
+# Define what should be imported as * when a client writes::
+#   from snail import *
+__all__ = []
---
+++ tests/test_init.py
@@ -0,0 +1,8 @@
+"""Test core objects/concepts
+"""
+import snail
+
+def test_init():
+    """Should create an empty network
+    """
+    pass
