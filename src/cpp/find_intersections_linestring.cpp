383274cbaab8ea8ef3632de20c8c7ca9eb0e3989
Thibault Lestang <thibault.lestang@cs.ox.ac.uk>
2021-06-23 13:28:19 +0100

    use alias type for linestring - #2

--- src/cpp/find_intersections_linestring.cpp
+++ src/cpp/find_intersections_linestring.cpp
@@ -2,17 +2,19 @@
 #include "geom.h"
 #include "raster.h"
 
+using linestr = std::vector<geometry::Vec2<double>>;
+
 // Splits a Line2 LINE into pieces contained within cells of raster data RASTER.
 // LineString being splitted SPLIT_LINESTRING ends at intersection point.
-std::vector<std::vector<geometry::Vec2<double>>> split(Ascii raster, geometry::Line2<double> line, std::vector<geometry::Vec2<double>> &split_linestring) {
+std::vector<linestr> split(Ascii raster, geometry::Line2<double> line, linestr &split_linestring) {
   // Recover the points of intersection of the line with the grid / graticule lines...
-  std::vector<geometry::Vec2<double>> intersections = raster.findIntersections(line);
+  linestr intersections = raster.findIntersections(line);
 
   // Add the start of the line to the cleaned feature...
   split_linestring.push_back(line.start);
 
   // Loop over each intersection, and add a new feature for each...
-  std::vector<std::vector<geometry::Vec2<double>>> splits;
+  std::vector<linestr> splits;
   for(std::size_t j=1; j<intersections.size(); j++){
     // Add the crossing point to the cleaned features geometry...
     split_linestring.push_back(intersections.at(j));
@@ -23,17 +25,17 @@ std::vector<std::vector<geometry::Vec2<double>>> split(Ascii raster, geometry::L
   return(splits);
 }
 
-std::vector<std::vector<geometry::Vec2<double>>> findIntersectionsLineString(Feature feature, Ascii raster) {
-  std::vector<geometry::Vec2<double>> linestring = feature.geometry;
+std::vector<linestr> findIntersectionsLineString(Feature feature, Ascii raster) {
+  linestr linestring = feature.geometry;
 
-  std::vector<std::vector<geometry::Vec2<double>>> split_linestrings;
-  std::vector<geometry::Vec2<double>> split_linestring;
+  std::vector<linestr> split_linestrings;
+  linestr split_linestring;
   for(std::size_t i=0; i<linestring.size()-1; i++){
     geometry::Line2<double> line(linestring.at(i), linestring.at(i+1));
     
     // If the line starts and ends in different cells, it needs to be cleaned...
     if(raster.cellIndex(line.start) != raster.cellIndex(line.end)){
-      std::vector<std::vector<geometry::Vec2<double>>> splits = split(raster, line, split_linestring);
+      std::vector<linestr> splits = split(raster, line, split_linestring);
       split_linestrings.insert(split_linestrings.end(), splits.begin(), splits.end());
     } else {
       split_linestring.push_back(linestring.at(i));
