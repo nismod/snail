d1459fc8d78e09f591615e88908a61a0db4d0460
Thibault Lestang <thibault.lestang@cs.ox.ac.uk>
2021-07-13 12:20:35 +0100

    Use Catch2's generate macro to merge both test cases

--- tests/cpp/tests_intersections.cpp
+++ tests/cpp/tests_intersections.cpp
@@ -4,6 +4,7 @@
 #include <ostream>
 #include <sstream>
 #include <string>
+#include <tuple>
 
 #include "geofeatures.hpp"
 #include "geom.hpp"
@@ -15,97 +16,75 @@
 using linestr = std::vector<geometry::Vec2<double>>;
 
 TEST_CASE("LineStrings are decomposed", "[decomposition]") {
-  // Linestring points are marked by o:
-  // Intersection points are marked by (o):
-  // +---------------+--------------+
-  // |               |              |
-  // |               |              |
-  // |               |              |
-  // |               |       o      |
-  // |               |       |      |
-  // |               |       |      |
-  // |               |       |      |
-  // +---------------+------(o)-----+
-  // |               |       |      |
-  // |               |       |      |
-  // |               |       |      |
-  // |       o---o--(o)------o      |
-  // |               |              |
-  // |               |              |
-  // |               |              |
-  // +---------------+--------------+
-  // (0,0)         (1,0)          (2,0)
 
-  geometry::Vec2<double> point1(0.5, 0.5);
-  geometry::Vec2<double> point2(0.75, 0.5);
-  geometry::Vec2<double> point3(1.5, 0.5);
-  geometry::Vec2<double> point4(1.5, 1.5);
+  auto data = GENERATE(table<linestr, std::vector<linestr>>({
+	{
+	  // Linestring points are marked by o:
+	  // Intersection points are marked by (o):
+	  // +---------------+--------------+
+	  // |               |              |
+	  // |               |              |
+	  // |               |              |
+	  // |               |       o      |
+	  // |               |       |      |
+	  // |               |       |      |
+	  // |               |       |      |
+	  // +---------------+------(o)-----+
+	  // |               |       |      |
+	  // |               |       |      |
+	  // |               |       |      |
+	  // |       o---o--(o)------o      |
+	  // |               |              |
+	  // |               |              |
+	  // |               |              |
+	  // +---------------+--------------+
+	  // (0,0)         (1,0)          (2,0)
+	  // Linestring
+	  {{0.5, 0.5}, {0.75, 0.5}, {1.5, 0.5}, {1.5, 1.5}},
+	  // Expected vector of splits
+	  {
+	    {{0.5, 0.5}, {0.75, 0.5},{1., 0.5}},
+	    {{1., 0.5},{1.5, 0.5},{1.5, 1.}},
+	    {{1.5, 1.}, {1.5, 1.5}}
+	  }
+	},
+	{
+	  // Linestring points are marked by o:
+	  // Intersection points are marked by (o):
+	  // +---------------+--------------+
+	  // |               |              |
+	  // |               |              |
+	  // |               |              |
+	  // |               |       o      |
+	  // |               |      /       |
+	  // |               |    /-        |
+	  // |               |  /-          |
+	  // +---------------+(o)-----------+
+	  // |              (o)             |
+	  // |             /-|              |
+	  // |            /  |              |
+	  // |       o---o   |              |
+	  // |               |              |
+	  // |               |              |
+	  // |               |              |
+	  // +---------------+--------------+
+	  // (0,0)         (1,0)          (2,0)
+	  // Linestring
+	  {{0.5, 0.5}, {0.75, 0.5}, {1.5, 1.5}},
+	  // Expected vector of splits
+	  {
+	    {{0.5, 0.5}, {0.75, 0.5}, {1., 0.8333}},
+	    {{1., 0.8333}, {1.125, 1.}},
+	    {{1.125, 1.}, {1.5, 1.5}}
+	  }
+	}
+      })
+    );
 
-  geometry::Vec2<double> inter1(1., 0.5);
-  geometry::Vec2<double> inter2(1.5, 1.);
-
-  std::vector<linestr> expected_splits = {
-      {point1, point2, inter1}, {inter1, point3, inter2}, {inter2, point4}};
-
-  Feature f;
-  linestr geom = {point1, point2, point3, point4};
-  f.geometry.insert(f.geometry.begin(), geom.begin(), geom.end());
-
-  Ascii test_raster("./tests/test_data/fake_raster.asc");
-  std::vector<linestr> splits = findIntersectionsLineString(f, test_raster);
-
-  // Test that we're getting the expected number of splits
-  REQUIRE(splits.size() == expected_splits.size());
-  // Test that each one of the splits have the expected size
-  for (int i = 0; i < splits.size(); i++) {
-    REQUIRE(splits[i].size() == expected_splits[i].size());
-  }
-  // Test that each one of the splits are made of the expected points
-  for (int i = 0; i < splits.size(); i++) {
-    for (int j = 0; j < splits[i].size(); j++) {
-      geometry::Vec2<double> point = splits[i][j];
-      geometry::Vec2<double> expected_point = expected_splits[i][j];
-
-      REQUIRE(std::abs(point.x - expected_point.x) < TOL);
-      REQUIRE(std::abs(point.y - expected_point.y) < TOL);
-    }
-  }
-}
-
-TEST_CASE("LineStrings are decomposed II", "[decomposition]") {
-  // Linestring points are marked by o:
-  // Intersection points are marked by (o):
-  // +---------------+--------------+
-  // |               |              |
-  // |               |              |
-  // |               |              |
-  // |               |       o      |
-  // |               |      /       |
-  // |               |    /-        |
-  // |               |  /-          |
-  // +---------------+(o)-----------+
-  // |              (o)             |
-  // |             /-|              |
-  // |            /  |              |
-  // |       o---o   |              |
-  // |               |              |
-  // |               |              |
-  // |               |              |
-  // +---------------+--------------+
-  // (0,0)         (1,0)          (2,0)
-
-  geometry::Vec2<double> point1(0.5, 0.5);
-  geometry::Vec2<double> point2(0.75, 0.5);
-  geometry::Vec2<double> point3(1.5, 1.5);
-
-  geometry::Vec2<double> inter1(1., 0.8333);
-  geometry::Vec2<double> inter2(1.125, 1.);
-
-  std::vector<linestr> expected_splits = {
-      {point1, point2, inter1}, {inter1, inter2}, {inter2, point3}};
+  std::vector<linestr> expected_splits = std::get<1>(data);
 
   Feature f;
-  linestr geom = {point1, point2, point3};
+  linestr geom = std::get<0>(data);
   f.geometry.insert(f.geometry.begin(), geom.begin(), geom.end());
 
   Ascii test_raster("./tests/test_data/fake_raster.asc");
