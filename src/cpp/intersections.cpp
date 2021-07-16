17ee3c94944f72f294446e47d6857a03af9a5926
Thibault Lestang <thibault.lestang@cs.ox.ac.uk>
2021-07-16 15:20:42 +0100

    [format] clang-format

--- src/cpp/intersections.cpp
+++ src/cpp/intersections.cpp
@@ -18,27 +18,25 @@ namespace geo = geometry;
 
 using linestr = std::vector<geometry::Vec2<double>>;
 
-std::vector <py::object> fun(py::object linestring_frompy, int nrows, int ncols, std::vector<double> transform) {
+std::vector<py::object> fun(py::object linestring_frompy, int nrows, int ncols,
+                            std::vector<double> transform) {
   py::object coords = linestring_frompy.attr("coords");
   int size = py::len(coords);
   std::vector<geo::Vec2<double>> linestring;
-  for (int i = 0; i<size;i++) {
+  for (int i = 0; i < size; i++) {
     py::tuple xy = (py::tuple)coords[py::cast(i)];
     geo::Vec2<double> p((py::float_)xy[0], (py::float_)xy[1]);
     linestring.push_back(p);
   }
-  Affine affine(transform[0],
-		transform[1],
-		transform[2],
-		transform[3],
-		transform[4],
-		transform[5]);
+  Affine affine(transform[0], transform[1], transform[2], transform[3],
+                transform[4], transform[5]);
   Grid grid(ncols, nrows, affine);
   Feature f;
   f.geometry.insert(f.geometry.begin(), linestring.begin(), linestring.end());
   std::vector<linestr> splits = findIntersectionsLineString(f, grid);
 
-  py::object shapely_linstr = py::module_::import("shapely.geometry").attr("LineString");
+  py::object shapely_linstr =
+      py::module_::import("shapely.geometry").attr("LineString");
   std::vector<py::object> splits_py;
   std::vector<std::vector<double>> split_py;
   std::vector<double> point_py;
