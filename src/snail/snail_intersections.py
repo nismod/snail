c4df1e878e040b81eb4237a03eafd08bba31d4d9
Thibault Lestang <thibault.lestang@cs.ox.ac.uk>
2021-08-03 10:30:40 +0100

    Function 'snail_intersections.split' can now split both linestrings and polygons

--- src/snail/snail_intersections.py
+++ src/snail/snail_intersections.py
@@ -1,21 +1,36 @@
 import geopandas as gpd
+from shapely.geometry import LineString, Polygon
+from shapely.ops import polygonize
 
 from snail.intersections import split as split_one_geom
+from snail.intersections import split_polygon as split_one_polygon
 from snail.intersections import get_cell_indices
 
 
 def split(vector_data, raster_data):
     all_splits = []
     all_idx = []
-    for idx, linestring in zip(vector_data.index, vector_data["geometry"]):
-        splits = split_one_geom(
-            linestring,
-            raster_data.width,
-            raster_data.height,
-            list(raster_data.transform),
-        )
-        all_splits.extend(splits)
-        all_idx.extend([idx] * len(splits))
+    for idx, geom in zip(vector_data.index, vector_data["geometry"]):
+        if type(geom) == LineString:
+            split_geoms = split_one_geom(
+                geom,
+                raster_data.width,
+                raster_data.height,
+                list(raster_data.transform),
+            )
+        elif type(geom) == Polygon:
+            splits = split_one_polygon(
+                geom,
+                raster_data.width,
+                raster_data.height,
+                list(raster_data.transform),
+            )
+            split_geoms = list(polygonize(splits))
+        else:
+            msg = f"""Incorrect geometry type {type(geom)}: only LineString and Polygon are supported."""
+            raise ValueError(msg)
+        all_splits.extend(split_geoms)
+        all_idx.extend([idx] * len(split_geoms))
 
     return gpd.GeoDataFrame({"line index": all_idx, "geometry": all_splits})
 
