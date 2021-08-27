5dbbcfb047c64d749a2ba2e79f6c3af9e058f9a9
Tom Russell <tomalrussell@gmail.com>
2021-08-27 12:06:06 +0100

    Initial unit test around splitAlongGridlines

--- tests/cpp/tests_intersections.cpp
+++ tests/cpp/tests_intersections.cpp
@@ -129,13 +129,92 @@ TEST_CASE("LineStrings are decomposed", "[decomposition]") {
 
   std::vector<linestr> expected_splits = test_data.expected_splits;
 
-  linestr geom = test_data.linestring;;
+  linestr geom = test_data.linestring;
   snail::geometry::LineString<double> line(geom);
 
   // Using default Affine transform(1, 0, 0, 0, 1, 0)
   snail::grid::Grid test_raster(2, 2, snail::transform::Affine());
   std::vector<linestr> splits = snail::operations::findIntersectionsLineString(line, test_raster);
 
+  // Test that we're getting the expected number of splits
+  REQUIRE(splits.size() == expected_splits.size());
+  // Test that each one of the splits have the expected size
+  for (int i = 0; i < splits.size(); i++) {
+    REQUIRE(splits[i].size() == expected_splits[i].size());
+  }
+  // Test that each one of the splits are made of the expected points
+  for (int i = 0; i < splits.size(); i++) {
+    for (int j = 0; j < splits[i].size(); j++) {
+      snail::geometry::Vec2<double> point = splits[i][j];
+      snail::geometry::Vec2<double> expected_point = expected_splits[i][j];
+
+      REQUIRE(std::abs(point.x - expected_point.x) < TOL);
+      REQUIRE(std::abs(point.y - expected_point.y) < TOL);
+    }
+  }
+}
+
+struct SplitGridConfig {
+  linestr exterior_crossings;
+  std::vector<linestr> expected_splits;
+  int min_level = 0;
+  int max_level = 2;
+  std::string direction = "horizontal";
+};
+
+
+TEST_CASE("Exterior ring splits to gridlines", "[decomposition]") {
+  // Linestring points are marked by o:
+  // Intersection points are marked by (o):
+  // Gridline segments are marked by = and ‖
+  // +---------------+--------------+
+  // |               |              |
+  // |               |              |
+  // |               |              |
+  // |               |       o      |
+  // |               |     / |      |
+  // |               |   /   |      |
+  // |               | /     |      |
+  // +--------------(o)=====(o)-----+
+  // |             / ‖       |      |
+  // |           /   ‖       |      |
+  // |         /     ‖       |      |
+  // |       o------(o)------o      |
+  // |               |              |
+  // |               |              |
+  // |               |              |
+  // +---------------+--------------+
+  // (0,0)         (1,0)          (2,0)
+  SplitGridConfig case1;
+  case1.exterior_crossings = {
+    {0.5, 0.5}, {1., 0.5}, {1.5, 1.}, {1., 1.}
+  };
+  case1.expected_splits = {
+    {{1.5, 1.}, {1., 1.}}, // TODO check, would actually expect left-to-right
+  };
+  SplitGridConfig case2;
+  case2.exterior_crossings = {
+    {0.5, 0.5}, {1., 0.5}, {1.5, 1.}, {1., 1.}
+  };
+  case2.expected_splits = {
+    {{1., 0.5}, {1., 1.}},
+  };
+  case2.direction = "vertical";
+
+  auto test_data = GENERATE_COPY(case1, case2);
+
+  std::vector<linestr> expected_splits = test_data.expected_splits;
+
+  // Using default Affine transform(1, 0, 0, 0, 1, 0)
+  snail::grid::Grid grid(2, 2, snail::transform::Affine());
+
+  std::vector<linestr> splits = snail::operations::splitAlongGridlines(
+    test_data.exterior_crossings,
+    test_data.min_level,
+    test_data.max_level,
+    test_data.direction,
+    grid
+  );
   // Test that we're getting the expected number of splits
   REQUIRE(splits.size() == expected_splits.size());
   // Test that each one of the splits have the expected size
