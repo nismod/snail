3ea72298cf1e298a0d1ca5dda2f58c8363e981d2
Thibault Lestang <thibault.lestang@cs.ox.ac.uk>
2021-08-29 18:27:55 +0100

    Draft computing single source shortest path for jamaica dataset

--- scripts/sources_sinks_routing/benchmark_shortest_path.py
+++ scripts/sources_sinks_routing/benchmark_shortest_path.py
@@ -1,12 +1,24 @@
-from igraph import Graph, plot
+from igraph import Graph
+import geopandas as gpd
+import matplotlib.pyplot as plt
 
-N = 10
-M = 10
-g = Graph.Lattice(dim=[N, M], circular=False)
+gdf = gpd.read_file("jamaica_roads.gpkg")
+edges = gdf.loc[:, ["from_node", "to_node", "length_km"]]
 
-sp = g.get_shortest_paths(20, 35)
-layout = g.layout_grid()
+g = Graph.DataFrame(edges, directed=False)
 
+start = g.vs.find(name="roadn_0")
+finish = g.vs.find(name="roadn_15891")
+
+# Return list of list of edge IDs
+sp = g.get_shortest_paths(
+    start.index, finish.index, weights="length_km", output="epath"
+)
+
+base = gdf.plot()
 for path in sp:
-    g.vs[path]["color"] = "blue"
-plot(g, layout=layout)
+    # Select linestrings according to edge IDs making the path
+    # edge ID are 1to1 with row id in dataframe
+    sub_gdf = gdf.iloc[path, :]
+    sub_gdf.plot(ax=base, color="green")
+plt.show()
