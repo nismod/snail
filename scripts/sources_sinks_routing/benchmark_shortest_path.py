fc29fceb3075bdbb0ba7565d7d5a7e17e853f025
Thibault Lestang <thibault.lestang@cs.ox.ac.uk>
2021-08-31 15:55:33 +0100

    Try to compare pandana and igraph shortest path over many to many shortest path calc

--- scripts/sources_sinks_routing/benchmark_shortest_path.py
+++ scripts/sources_sinks_routing/benchmark_shortest_path.py
@@ -1,3 +1,5 @@
+import random
+
 import pandana
 from pandas import DataFrame
 import numpy as np
@@ -6,35 +8,50 @@ import geopandas as gpd
 
 gdf = gpd.read_file("jamaica_roads.gpkg")
 
+# Transform source and destination columns into lists of integers
+# (node ids)
+from_node = [int(node_id[6:]) for node_id in gdf["from_node"]]
+to_node = [int(node_id[6:]) for node_id in gdf["to_node"]]
+
+# Array of all node ids
+node_ids, geodf_idxs = np.unique(from_node + to_node, return_index=True)
+
+# Sample source and desitnation ensembles
+Ns = 10
+Nt = 10
+source_ensbl = random.sample(list(node_ids), Ns)
+dest_ensbl = random.sample(list(node_ids), Nt)
+
 # ########## python-igraph ##########
 
 edges = gdf.loc[:, ["from_node", "to_node", "length_km"]]
 g = Graph.DataFrame(edges, directed=False)
-start = g.vs.find(name="roadn_0")
-finish = g.vs.find(name="roadn_15891")
-sp_igraph = g.get_shortest_paths(
-    start.index, finish.index, weights="length_km", output="vpath"
-)
+dest = g.vs.select(name_in=["roadn_" + str(i) for i in dest_ensbl])
+
+for source_id in source_ensbl:
+    start = g.vs.find(name="roadn_" + str(source_id))
+    sp_igraph = g.get_shortest_paths(
+        start, dest, weights="length_km", output="vpath"
+    )
+
 
 # ########## pandana ##########
 
-from_node = [int(node_id[6:]) for node_id in gdf["from_node"]]
-to_node = [int(node_id[6:]) for node_id in gdf["to_node"]]
-
-nids, idxs = np.unique(from_node + to_node, return_index=True)
+# Build arrays of coordinate values
 x = []
 y = []
-for nid, idx in zip(nids, idxs):
-    linestr = gdf.loc[idx % len(from_node), "geometry"]
-    coords = linestr.coords[int(-1 * (idx // len(from_node)))]
+for geodf_idx in geodf_idxs:
+    linestr = gdf.loc[geodf_idx % len(from_node), "geometry"]
+    coords = linestr.coords[int(-1 * (geodf_idx // len(from_node)))]
     x.append(coords[0])
     y.append(coords[1])
 
-nodes = DataFrame({"x": x, "y": y}, index=nids)
+nodes = DataFrame({"x": x, "y": y}, index=node_ids)
 edges = DataFrame(
     {"from": from_node, "to": to_node, "weight": gdf["length_km"].values}
 )
 net = pandana.Network(
     nodes["x"], nodes["y"], edges["from"], edges["to"], edges[["weight"]]
 )
-sp_pandana = net.shortest_path(0, 15981)
+
+sp_pandana = net.shortest_paths(source_ensbl * len(dest_ensbl), dest_ensbl)
