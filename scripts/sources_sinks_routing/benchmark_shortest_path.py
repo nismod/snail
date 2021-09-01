fb00bd68b1e47d2276fc372545f022d9f5c3594e
Thibault Lestang <thibault.lestang@cs.ox.ac.uk>
2021-09-01 09:38:02 +0100

    Only convert node ids to integers before using pandana

--- scripts/sources_sinks_routing/benchmark_shortest_path.py
+++ scripts/sources_sinks_routing/benchmark_shortest_path.py
@@ -7,39 +7,36 @@ from igraph import Graph
 import geopandas as gpd
 
 gdf = gpd.read_file("jamaica_roads.gpkg")
+nodes_gdf = gpd.read_file("jamaica_roads.gpkg", layer="nodes")
+
+# Sample source and desitnation ensembles
+Ns = 10
+Nt = 10
+source_ensbl = random.sample(nodes_gdf["node_id"], Ns)
+dest_ensbl = random.sample(nodes_gdf["node_id"], Nt)
+
+# ########## python-igraph ##########
+
+edges = gdf.loc[:, ["from_node", "to_node", "length_km"]]
+g = Graph.DataFrame(edges, directed=False)
+dest = g.vs.select(name_in=dest_ensbl)
+
+for source_id in source_ensbl:
+    start = g.vs.find(name=source_id)
+    sp_igraph = g.get_shortest_paths(
+        start, dest, weights="length_km", output="vpath"
+    )
+
+# ########## pandana ##########
+
+list_of_coords = [(point.x, point.y) for point in nodes_gdf.loc[:, "geometry"]]
+nodes = DataFrame(list_of_coords, columns=["x", "y"])
 
 # Transform source and destination columns into lists of integers
 # (node ids)
 from_node = [int(node_id[6:]) for node_id in gdf["from_node"]]
 to_node = [int(node_id[6:]) for node_id in gdf["to_node"]]
 
-# Array of all node ids
-node_ids, geodf_idxs = np.unique(from_node + to_node, return_index=True)
-
-# Sample source and desitnation ensembles
-Ns = 10
-Nt = 10
-source_ensbl = random.sample(list(node_ids), Ns)
-dest_ensbl = random.sample(list(node_ids), Nt)
-
-# ########## python-igraph ##########
-
-edges = gdf.loc[:, ["from_node", "to_node", "length_km"]]
-g = Graph.DataFrame(edges, directed=False)
-dest = g.vs.select(name_in=["roadn_" + str(i) for i in dest_ensbl])
-
-for source_id in source_ensbl:
-    start = g.vs.find(name="roadn_" + str(source_id))
-    sp_igraph = g.get_shortest_paths(
-        start, dest, weights="length_km", output="vpath"
-    )
-
-
-# ########## pandana ##########
-
-nodes_gdf = gpd.read_file("jamaica_roads.gpkg", layer="nodes")
-list_of_coords = [(point.x, point.y) for point in nodes_gdf.loc[:, "geometry"]]
-nodes = DataFrame(list_of_coords, columns=["x", "y"])
 edges = DataFrame(
     {"from": from_node, "to": to_node, "weight": gdf["length_km"].values}
 )
