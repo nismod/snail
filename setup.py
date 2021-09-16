f03e61899773024458ea93e571d704c9bdb29e22
Thibault Lestang <thibault.lestang@cs.ox.ac.uk>
2021-09-16 17:00:15 +0100

    Add igraph as a dependency

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
         'setuptools_scm'
     ],
     install_requires=[
-        'affine', 'numpy', 'geopandas', 'shapely', 'rasterio'
+        'affine', 'numpy', 'geopandas', 'shapely', 'rasterio', 'igraph'
     ],
     extras_require={
         # eg:
