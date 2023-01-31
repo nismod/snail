d451c4954e58fcc0e6e0a4d52278f1677ad73ec7
Tom Russell <tom.russell@ouce.ox.ac.uk>
2023-01-31 12:33:24 +0000

    Format

--- tests/test_routing.py
+++ tests/test_routing.py
@@ -20,10 +20,8 @@ class TestSnailRouting(unittest.TestCase):
         g = Graph.Ring(n=5, circular=True)
         g.vs["name"] = ["node_" + str(i) for i in range(5)]
         g.es["length_km"] = [1, 1, 1, 3, 1]
-        sps = shortest_paths(["node_0", "node_2"], ["node_4", "node_3"], g, "length_km")
-        expected_paths = [
-            [4], [0, 1, 2], [1, 0, 4], [2]
-            ]
+        sps = shortest_paths(
+            ["node_0", "node_2"], ["node_4", "node_3"], g, "length_km"
+        )
+        expected_paths = [[4], [0, 1, 2], [1, 0, 4], [2]]
         self.assertEqual(sps, expected_paths)
-
-        
